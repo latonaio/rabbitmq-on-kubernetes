83277fb407fba9ad19ff642dce41bde76e8f949e
MiyuYokoyama0109 <miyu.y@latona.io>
2022-11-04 18:39:20 +0900

    release

--- init-queues.sh
+++ init-queues.sh
@@ -5,23 +5,82 @@
 ####################
 
 # ユーザ名
-user=sample_user
+user=guest
 
 # パスワード
-passwd=sample_pass
+passwd=guest
 
 # バーチャルホスト名
-vhost=sample_host
+vhost=sap-test
 
 # キュー名一覧
 queues=(
-	azure-face-api-identifier-kube-queue
-	azure-face-api-registrator-kube-queue
-	get-response-of-face-api-kube-queue
-	register-face-to-guest-table-kube-queue
-	send-data-to-azure-iot-hub-queue
-	slack-message-client-kube-queue
-	avis-client-kube-queue
+	sap-api-integrations-classification-reads-rmq-kube
+	sap-api-integrations-characteristic-reads-rmq-kube
+	sap-api-integrations-bank-master-reads-rmq-kube
+	sap-api-integrations-business-partner-reads-rmq-kube
+	sap-api-integrations-product-group-reads-rmq-kube
+	sap-api-integrations-product-master-reads-rmq-kube
+	sap-api-integrations-product-master-class-reads-rmq-kube
+	sap-api-integrations-batch-master-record-reads-rmq-kube
+	sap-api-integrations-material-stock-reads-rmq-kube
+	sap-api-integrations-reservation-document-reads-rmq-kube
+	sap-api-integrations-inbound-delivery-reads-rmq-kube
+	sap-api-integrations-material-document-reads-rmq-kube
+	sap-api-integrations-physical-inventory-doc-reads-rmq-kube
+	sap-api-integrations-warehouse-resource-reads-rmq-kube
+	sap-api-integrations-warehouse-available-stock-reads-rmq-kube
+	sap-api-integrations-business-partner-reads-customer-rmq-kube
+	sap-api-integrations-credit-mgmt-master-reads-rmq-kube
+	sap-api-integrations-customer-material-reads-rmq-kube
+	sap-api-integrations-sales-pricing-reads-rmq-kube	
+	sap-api-integrations-sales-inquiry-reads-rmq-kube
+	sap-api-integrations-sales-quotation-reads-rmq-kube
+	sap-api-integrations-sales-order-reads-rmq-kube
+	sap-api-integrations-sales-contract-reads-rmq-kube
+	sap-api-integrations-sales-scheduling-agreement-reads-rmq-kube
+	sap-api-integrations-outbound-delivery-reads-rmq-kube
+	sap-api-integrations-customer-return-reads-rmq-kube
+	sap-api-integrations-billing-document-reads-rmq-kube
+	sap-api-integrations-credit-memo-request-reads-rmq-kube
+	sap-api-integrations-debit-memo-request-reads-rmq-kube
+	sap-api-integrations-business-partner-reads-supplier-rmq-kube
+	sap-api-integrations-pur-source-list-reads-rmq-kube
+	sap-api-integrations-pur-info-record-reads-rmq-kube
+	sap-api-integrations-purchase-requisition-reads-rmq-kube
+	sap-api-integrations-purchase-order-reads-rmq-kube
+	sap-api-integrations-purchase-contract-reads-rmq-kube
+	sap-api-integrations-purchase-scheduling--reads-rmq-kube
+	sap-api-integrations-supplier-invoice-reads-rmq-kube
+	sap-api-integrations-inbound-delivery-reads-rmq-kube
+	sap-api-integrations-bill-of-material-reads-rmq-kube
+	sap-api-integrations-bom-where-used-list-reads-rmq-kube
+	sap-api-integrations-work-center-reads-rmq-kube
+	sap-api-integrations-production-routing-reads-rmq-kube
+	sap-api-integrations-planned-independent-req-reads-rmq-kube
+	sap-api-integrations-planned-order-reads-rmq-kube
+	sap-api-integrations-production-order-reads-rmq-kube
+	sap-api-integrations-production-order-conf-reads-rmq-kube
+	sap-api-integrations-master-recipe-reads-rmq-kube
+    sap-api-integrations-process-order-reads-rmq-kube
+	sap-api-integrations-process-order-conf-reads-rmq-kube
+    sap-api-integrations-inspection-plan-reads-rmq-kube
+	sap-api-integrations-quality-info-record-reads-rmq-kube
+	sap-api-integrations-functional-location-reads-rmq-kube
+	sap-api-integrations-equipment-master-reads-rmq-kube
+	sap-api-integrations-maintenance-bom-reads-rmq-kube
+	sap-api-integrations-maintenance-plan-reads-rmq-kube
+	sap-api-integrations-maintenance-item-reads-rmq-kube
+	sap-api-integrations-maintenance-notification-reads-rmq-kube
+	sap-api-integrations-maintenance-order-reads-rmq-kube
+	sap-api-integrations-maintenance-order-conf-reads-rmq-kube
+	sap-api-integrations-defect-reads-rmq-kube
+	sap-api-integrations-maintenance-task-list-reads-rmq-kube
+    sap-api-integrations-measuring-point-reads-rmq-kube
+	sap-api-integrations-measurement-document-reads-rmq-kube
+	sap-api-integrations-service-order-reads-rmq-kube
+	sap-api-integrations-service-confirmation-reads-rmq-kube
+	sap-sql-update-kube
 )
 
 
